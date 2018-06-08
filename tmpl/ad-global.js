4be311a93c705bb21f134ee4c62374947711e680
Kenny Arehart <kenny.arehart@ff0000.com>
2018-06-08 12:02:47 -0700

    updated dist process, simplified to have entry point top level

--- tmpl/ad-global.js
+++
@@ -1,5 +0,0 @@
-/*-- Red.Inject.ad-global.start --*/
-/*-- Red.Inject.ad-global.end --*/
-;['getDeployProfile', 'getQueryParams', 'matchProtocolTo', 'isVersionOrNewer'].forEach(method => {
-	window[method] = adGlobal[method]
-})
